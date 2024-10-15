54cd74135e787e5d4bb975531b15f3d1e8eff2c9
ronaldmartin02 <ronaldmartin02@gmail.com>
2024-10-15 00:47:42 -0400

    some styling

--- Client/src/pages/shop.jsx
+++ Client/src/pages/shop.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Shop = () => {
-    return (
-        <div>
-            <h1>Shop Page</h1>
-            {/* Add your shop content here */}
-            <p>Welcome to our shop!</p>
-            <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
-            </ul>
-        </div>
-    );
-};
 
-export default Shop;
+export default function Shop(){
+return (
+    <div>
+        <h1>Shop Page</h1>
+        {/* Add your shop content here */}
+        <p>Welcome to our shop!</p>
+        <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+        </ul>
+    </div>
+);
+};
