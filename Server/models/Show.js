868f78bf15286e14f2864d26ac40a4031a547810
ronaldmartin02 <ronaldmartin02@gmail.com>
2024-10-15 23:22:00 -0400

    added to show schema

--- Server/models/Show.js
+++ Server/models/Show.js
@@ -1,5 +1,26 @@
 const { Schema, model } = require('mongoose');
 
+const wrestlersSchema = new Schema({
+    wrestlerName: {
+        type: String,
+        required: true
+    },
+});
+const matchSchema = new Schema({
+matchType: {
+    type: String,
+    required: true
+},
+Wrestlers:[wrestlersSchema],
+matchWinner: {
+    type: String,
+    required: true
+},
+videoId: {
+    type: String,
+    required: true
+}
+});
 const showSchema = new Schema({
     showBanner: {
         type: String,
@@ -9,6 +30,23 @@ const showSchema = new Schema({
         type: Date,
         required: true
     },
+    showTime: {
+        type: String,
+        required: true
+    },
+    showLocation: {
+        type: String,
+        required: true
+    },
+    showName: {
+        type: String,
+        required: true
+    },
+    showPrice: {
+        type: Number,
+        required: true
+    },
+    results: [matchSchema]
 });
 
 const Show = model('Show', showSchema);
